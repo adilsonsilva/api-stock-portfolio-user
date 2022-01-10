46886e6b3ce49128db74de5e11094e8fe14d0f1a
adilsonsilva <adilson.ro.silva@gmail.com>
2022-01-10 07:29:08 -0300

    Add teste unitario criar e deletar

--- src/test/java/br/com/archtype/service/impl/UserServiceTest.java
+++ src/test/java/br/com/archtype/service/impl/UserServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.archtype.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,5 +25,32 @@ class UserServiceTest {
 		Long qtdUser = users.stream().count();
 		assertEquals(10, qtdUser);
 	}
+	
+	@Test
+	void getInsertUser() {
+		try {
+			User user = User.builder()
+					.cpf("09876523410")
+					.email("unittest@teste.com.br")
+					.fullName("Unit teste")
+					.password("123456")
+					.surName("teste")
+					.build();
+			
+			userService.createUser(user);
+		}catch (Exception e) {
+			fail();
+		}
+	}
 
+	@Test
+	void getDeleteUser() {
+		try {
+			userService.deleteUser(1);
+		}catch (Exception e) {
+			fail();
+		}
+	}
+	
+	
 }
